15d21d8190593ba1317eccc572c08612c1d2f0b2
PlasticStrawActivist <aiden.landy@gmail.com>
2025-09-30 13:01:48 +1000

    Improve formatting in execptions demos program

--- prac_03/exceptions_demos.py
+++ prac_03/exceptions_demos.py
@@ -1,8 +1,10 @@
 """
 CP1404/CP5632 - Practical
 Answer the following questions:
-1. When will a ValueError occur? When the user input can't be converted to an int.
-2. When will a ZeroDivisionError occur? When the denominator varible is 0.
+1. When will a ValueError occur?
+    When the user input can't be converted to an int.
+2. When will a ZeroDivisionError occur?
+    When the denominator varible is 0.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
     Check if the denominator varible is 0 before the division is executed.
 """
