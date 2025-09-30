34b2c906c4356c8c194f42ad9a393ecb3b4d3318
PlasticStrawActivist <aiden.landy@gmail.com>
2025-09-30 13:24:05 +1000

    Implement TODO comments in password checker program

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -15,7 +15,7 @@ def main():
     print(
         f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:"
     )
-    print("\t1 or more uppercase characters")
+    print(f"\t or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
     if IS_SPECIAL_CHARACTER_REQUIRED:
@@ -30,19 +30,28 @@ def main():
 def is_valid_password(password):
     """Determine if the provided password is valid."""
     # TODO: if length is wrong, return False
+    if not MIN_LENGTH <= len(password) <= MAX_LENGTH:
+        return False
 
     number_of_lower = 0
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        number_of_lower += int(character.islower())
+        number_of_upper += int(character.isupper())
+        number_of_digit += int(character.isdigit())
+        number_of_special += int(character in SPECIAL_CHARACTERS)
+        # TODO: count each kind of character (use str methods like isdigit
 
     # TODO: if any of the 'normal' counts are zero, return False
+    if not number_of_lower or not number_of_upper or not number_of_digit:
+        return False
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
