1d4e933cf8a80172d980f6d90491d31f0b4f69f7
PlasticStrawActivist <aiden.landy@gmail.com>
2025-09-30 12:38:00 +1000

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 - Practical
+Various examples of using the random module.
+"""
+
+import random
+
+print(random.randint(5, 20))  # 18, 5, 20
+print(random.randrange(3, 10, 2))  # 3, 3, 9
+print(random.uniform(2.5, 5.5))  # 4.461285291755985, 2.5, 5.5
+
+print(random.randint(1, 100))
