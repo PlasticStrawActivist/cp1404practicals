7b01bb09c809adad66ca1751e9936911c4ad8829
PlasticStrawActivist <aiden.landy@gmail.com>
2025-10-12 21:43:17 +1000

    Complete second part of list exercises for prac 4

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,26 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The averge of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = [
+    "jimbo",
+    "giltson98",
+    "derekf",
+    "WhatSup",
+    "NicolEye",
+    "swei45",
+    "BaseInterpreterInterface",
+    "BaseStdIn",
+    "Command",
+    "ExecState",
+    "InteractiveConsole",
+    "InterpreterInterface",
+    "StartServer",
+    "bob",
+]
+
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
