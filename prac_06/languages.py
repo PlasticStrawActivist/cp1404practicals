24db8d36cf62808efd6e5e5a635a2b8e72a4294b
PlasticStrawActivist <aiden.landy@gmail.com>
2025-11-02 18:09:29 +1000

    Fix spelling mistake

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,11 +4,11 @@ Estimate: 2 minutes
 Actual:   2 mintues, 34 seconds
 """
 
-from prac_06.programming_language import ProgrammingLanguge
+from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguge("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguge("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguge("Visual Basic", "Static", False, 1991)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
 
 languages = [python, ruby, visual_basic]
