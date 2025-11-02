8e2ec3813e31c24b7bec5220eb774e0a516e0d5e
PlasticStrawActivist <aiden.landy@gmail.com>
2025-11-02 16:16:16 +1000

    Add guitar program for prac 6

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,7 +7,7 @@ Actual: 2 minutes, 30 seconds
 
 class Guitar:
 
-    def __init__(self, name="", year=0, cost=0) -> None:
+    def __init__(self, name="", year=0, cost=0.0) -> None:
         self.name = name
         self.year = year
         self.cost = cost
---
+++ prac_06/guitars.py
@@ -0,0 +1,46 @@
+"""
+Guitar
+Estimate: 20 minutes
+Actual:
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Guitar program"""
+    print("My guitars")
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        while True:
+            try:
+                year = int(input("Year: "))
+            except ValueError:
+                print("Year must be an int")
+                continue
+            else:
+                break
+
+        while True:
+            try:
+                cost = float(input("Cost: $"))
+            except ValueError:
+                print("Cost must be a float")
+                continue
+            else:
+                break
+
+        guitars.append(Guitar(name, year, cost))
+        print(f"{guitars[-1]} added.\n")
+        name = input("Name: ")
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(
+            f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}"
+        )
+
+
+main()
