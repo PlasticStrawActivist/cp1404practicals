428c44d029f09cc55436148e68a623d7bd21b4d4
PlasticStrawActivist <aiden.landy@gmail.com>
2025-11-02 17:59:38 +1000

    Add missing docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,17 +6,27 @@ Actual: 2 minutes, 30 seconds
 
 
 class Guitar:
+    """Represent a Guitar object."""
 
     def __init__(self, name="", year=0, cost=0.0) -> None:
+        """Initialise a Guitar instance.
+
+        name: float, name of guitar
+        year: int, year the guitar was made
+        cost: float, cost of guitar
+        """
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self) -> str:
+        """String method for Guitar object."""
         return "{} ({}) : ${}".format(self.name, self.year, self.cost)
 
     def get_age(self) -> int:
+        """String age of guitar."""
         return 2025 - self.year
 
     def is_vintage(self) -> bool:
+        """Return True if the age is greater than or equal to 50."""
         return self.get_age() >= 50
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,17 +6,26 @@ Actual:   1 minutes, 20 seconds
 
 
 class ProgrammingLanguge:
+    """Represent a Programming Language object."""
 
     def __init__(
         self, name: str, typing: str, reflection: bool, year: int
     ) -> None:
+        """Initialise a ProgrammingLanguge instance.
+
+        name: str, name of language
+        reflection: bool, if language has reflection
+        year: int, year of language
+        """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self) -> bool:
+        """Return True if 'typing' is equal to 'dynamic'."""
         return self.typing.lower() == "dynamic"
 
     def __str__(self) -> str:
+        """String method for ProgrammingLanguge object."""
         return f"{self.name}, {self.typing.title()} Typing, Reflection={self.reflection}, First appeared in {self.year}"
