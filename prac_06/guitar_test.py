be97d8036d2c57eff9c74a8a9a11a3b93ad9fa9a
PlasticStrawActivist <aiden.landy@gmail.com>
2025-11-02 15:57:33 +1000

    Add guitar test program for prac 6

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,20 @@
+"""
+Guitar Test
+Estimate: 4 minutes
+Actual:   6 minutes, 11 seconds
+"""
+
+from prac_06.guitar import Guitar
+
+guitars = [Guitar("Gibson L-5 CES", 1922), Guitar("Another Guitar", 2013)]
+
+
+for index, age in enumerate([103, 12]):
+    guitar = guitars[index]
+    print(f"{guitar.name} get_age(): Expected {age}. Got {guitar.get_age()}")
+
+for index, is_vintage in enumerate([True, False]):
+    guitar = guitars[index]
+    print(
+        f"{guitar.name} is_vintage(): Expected {is_vintage}. Got {guitar.is_vintage()}"
+    )
