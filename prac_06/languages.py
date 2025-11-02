76b3d0b53a9015a4339e4fd21eb0ebbadb8b12a5
PlasticStrawActivist <aiden.landy@gmail.com>
2025-11-02 15:27:38 +1000

    Add languages program

---
+++ prac_06/languages.py
@@ -0,0 +1,21 @@
+"""
+Languages
+Estimate: 2 minutes
+Actual: 2 mintues, 34 seconds
+"""
+
+from prac_06.programming_language import ProgrammingLanguge
+
+python = ProgrammingLanguge("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguge("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguge("Visual Basic", "Static", False, 1991)
+print(python)
+
+languages = [python, ruby, visual_basic]
+
+print("The dynamically typed languages are:")
+print(
+    "\n".join(
+        [language.name for language in languages if language.is_dynamic()]
+    )
+)
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,10 +7,16 @@ Actual:   1 minutes, 20 seconds
 
 class ProgrammingLanguge:
 
-    def __init__(self, typing: str, reflection: bool, year: int) -> None:
+    def __init__(
+        self, name: str, typing: str, reflection: bool, year: int
+    ) -> None:
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self) -> bool:
-        return self.typing == "dynamic"
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self) -> str:
+        return f"{self.name}, {self.typing.title()} Typing, Reflection={self.reflection}, First appeared in {self.year}"
